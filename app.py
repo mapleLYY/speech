671e0bbb0a0816f30b3d3b1c0f8e0daa5fd6749b
mapleLYY <99797466+mapleLYY@users.noreply.github.com>
2022-03-09 16:24:44 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+# from flask import Flask, render_template, request
+# from werkzeug.utils import secure_filename
+# import speech_recognition as sr
+
+# app = Flask(__name__)
+
+# @app.route('/', methods=['GET', 'POST'])
+# def index():
+#     if request.method == 'POST':
+#         file = request.files['file']
+#         print("File Received")
+#         filename = secure_filename(file.filename)
+#         file.save("static/" + filename)
+#         a = sr.AudioFile("static/" + filename)
+#         with a as source:
+#             a = sr.Recognizer().record(source)
+#         s = sr.Recognizer().recognize_google(a)
+#         return(render_template("index.html", result=s))
+#     else:
+#         return(render_template("index.html", result="2"))
+    
+# if __name__ == "__main__":
+#     app.run()
+
+
+# In[ ]:
+
+
+from flask import Flask, render_template, request
+from werkzeug.utils import secure_filename
+import speech_recognition as sr
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return(render_template("index.html"))
+
+@app.route("/predict", methods=["GET","POST"])
+def predict():
+    if request.method=="POST":
+        file = request.files["file"]
+        print("file received")
+        filename = secure_filename(file.filename)
+        print(filename)
+        file.save("static/"+filename)
+        a = sr.AudioFile("static/"+filename)
+        with a as source:
+            a = sr.Recognizer().record(source)
+        s = sr.Recognizer().recognize_google(a)
+        return(render_template("index.html", result=s))
+    else:
+        return(render_template("index.html", result="2"))
+    
+if __name__=="__main__":
+    app.run()
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
